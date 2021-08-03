06310e5e842834b6d9be8704953fa7dc1aff8b7b
Unique Maharjan <unique.maharzn@gmail.com>
2021-08-03 08:56:15 +0545

    second commit

--- to-do.py
+++ to-do.py
@@ -5,21 +5,20 @@ Date: 2021-07-20
 
 def Start():
     user = input('Do you want to create your to-do list?? (Y/N): ')
-
-    while True:
-        if user[0].lower() == 'y':
+    if user.lower() == 'n':
+        print('Thank you for using this!!')
+    while user.lower() == 'y':
             print('Please add your things to be done list below: ')
             user_input = ''
             user_data = []
             while user_input != None:
                 take_input = input()
                 user_data.append(take_input)
-                if take_input == 'Done':
+                if take_input.lower() == 'done':
                     endt = input('Are you done with giving list? (Y/N): ')
-                    if endt.lower() == 'Y':
-                        break
-        elif user[0].lower() == 'n':
-            print('Thank you !!')
+                    if endt.lower() == 'y':
+                        exit()
+
 
 
 
